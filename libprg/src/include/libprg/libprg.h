1b7d70ac5183993629816af38cf775959ad055db
evandroc-dev <evandro.c@aluno.ifsc.edu.br>
2025-10-27 19:47:24 -0300

    teste libprg

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -16,26 +16,26 @@ void imprimir(pilha_t* pilha);
 
 //Fila
 
-typedef struct fila fila_t;
-fila_t* criar_fila(int capacidade);
-bool cheia(fila_t* fila);
-bool fila_vazia(fila_t* fila);
-int fila_inicio(fila_t* fila);
-int fila_fim(fila_t* fila);
-int desenfileirar(fila_t* fila);
-int tamanho_fila(fila_t* fila);
-void destruir_fila(fila_t* fila);
-#endif
-
-//Lista
-typedef struct lista_linear lista_linear_t;
-lista_linear_t* criar_lista_linear(int capacidade, bool ordenada);
-void inserir(lista_linear_t* lista, int valor);
-bool lista_cheia(lista_linear_t* lista);
-
-//Lista encadeada
-typedef struct no no_t;
-no_t* criar_lista_encadeada (int valor);
-no_t* adicionar(no_t* inicio, int valor);
-no_t* buscar_lista_encadeada(no_t** inicio, int valor);
-void remover(no_t** inicio, int valor);
+// typedef struct fila fila_t;
+// fila_t* criar_fila(int capacidade);
+// bool cheia(fila_t* fila);
+// bool fila_vazia(fila_t* fila);
+// int fila_inicio(fila_t* fila);
+// int fila_fim(fila_t* fila);
+// int desenfileirar(fila_t* fila);
+// int tamanho_fila(fila_t* fila);
+// void destruir_fila(fila_t* fila);
+// #endif
+//
+// //Lista
+// typedef struct lista_linear lista_linear_t;
+// lista_linear_t* criar_lista_linear(int capacidade, bool ordenada);
+// void inserir(lista_linear_t* lista, int valor);
+// bool lista_cheia(lista_linear_t* lista);
+//
+// //Lista encadeada
+// typedef struct no no_t;
+// no_t* criar_lista_encadeada (int valor);
+// no_t* adicionar(no_t* inicio, int valor);
+// no_t* buscar_lista_encadeada(no_t** inicio, int valor);
+// void remover(no_t** inicio, int valor);
